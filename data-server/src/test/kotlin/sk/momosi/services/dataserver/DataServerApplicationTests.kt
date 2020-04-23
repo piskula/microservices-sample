848a5545eb2708e4b5f0502ec0ef835f4bbb8595
Ondrej Oravčok <pista.baci@atlas.sk>
2020-04-23 17:07:40 +0200

    update spring and kotlin

--- data-server/src/test/kotlin/sk/momosi/services/dataserver/DataServerApplicationTests.kt
+++ data-server/src/test/kotlin/sk/momosi/services/dataserver/DataServerApplicationTests.kt
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner
 class DataServerApplicationTests {
 
 //    @Test
-//    fun contextLoads() {
-//    }
+//    fun contextLoads() { }
 
 }
